b31ce4068bf5f6acf72704a34e53e9eec04dbe3d
Ayrton <ayrttoon@hotmail.com>
2022-05-03 16:14:19 -0300

    Build the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,58 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
+
+        <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    </head>
+</head>
+
+<body id="body">
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Login
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="text-input" id="email" name="email"
+                                aria-describedby="emailHelp" placeholder="Type your email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="text-input" id="password" name="password"
+                                placeholder="Type your password">
+                        </div>
+                        <div>
+                            <div id="password-remember">
+                                Remember me
+                                <input type="checkbox">
+                            </div>
+                            <p>
+                                <a id="forgotten-password" href="{{ route('password.request') }}">Forgot
+                                    your password?</a>
+                            </p>
+                        </div>
+                        <button type="submit" class="button">Sign In</button>
+                    </div>
+                </form>
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <div class="card-footer">
+                Don't have an account yet?&nbsp
+                <a href="{{ route('register') }}">Sign Up</a>
             </div>
+        </div>
+    </div>
+</body>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>
